099b26e2fdc0c8dd17dc6fe5ef80ac151635d9f7
Obolo Emmanuel <e.obolo@alustudent.com>
2023-07-06 16:23:10 -0700

    bug fixed again 9. To uppercase

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
 def uppercase(str):
-    separator = "\n"
-    if len(str) == 0:
-        print("{}".format(separator), end=separator)
-        return 1
-    output = ""
     for i in range(0, len(str)):
         value = ord(str[i])
         separator = "\n"
@@ -13,11 +8,11 @@ def uppercase(str):
         else:
             separator = "\n"
         if value < 97 or value > 122:
-            output += chr(value)
-            #print("{0:c}".format(value), end=separator)
+            print("{0:c}".format(value), end=separator)
         else:
             value = value - 32
-            output += chr(value)
-            #print("{0:c}".format(value), end=separator)
-    print("{0:s}".format(output), end=separator)
-    return 0
+            print("{0:c}".format(value), end=separator)
+    if len(str) == 0:
+        print("{}".format("\n"), end="\n")
+    else:
+        return 0
