a082f9930914508dbda3e15787996d3331101890
Obolo Emmanuel <e.obolo@alustudent.com>
2023-07-06 16:39:12 -0700

    bug refixed again 9. To uppercase

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -6,4 +6,4 @@ def uppercase(string):
         uppercase_char = chr(ascii_value - 32)\
             if 97 <= ascii_value <= 122 else char
         result += uppercase_char
-    print(f"{result}\n", end='')
+    print("{0:s}\n".format(result), end='')
