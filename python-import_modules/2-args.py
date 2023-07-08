46db043add62664774e7228da27a06a2f68bdc41
Obolo Emmanuel <e.obolo@alustudent.com>
2023-07-08 07:35:42 -0700

    bug fixed in 2. How to make a script dynamic!

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,13 +4,13 @@ if __name__ == "__main__":
     def args():
         length = len(sys.argv)
         if length == 1:
-            print("{0:d} arguements.".format(length - 1))
+            print("{0:d} arguments.".format(length - 1))
             return 1
 
         if length == 2:
-            print("{0:d} arguement:".format(length - 1))
+            print("{0:d} argument:".format(length - 1))
         else:
-            print("{0:d} arguements:".format(length - 1))
+            print("{0:d} arguments:".format(length - 1))
         for i in range(1, length):
             print("{0:d}: {1:s}".format(i, sys.argv[i]))
         return 0
