6a8f1ae133f6e8f65b1aca0d111dee1edb06ce59
Obolo Emmanuel <e.obolo@alustudent.com>
2023-07-14 12:52:43 -0700

    9. Find the max

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    # let maximum be first set to an negative number specifically
+    # since we want to find the maximum of a list
+    # we don't want a value for maximum to be larger than max in a list
+    maximum = -10
+    if my_list == "":
+        return None
+    for i in my_list:
+        if maximum > i:
+            continue
+        maximum = i
+    return maximum
