522182eb37a2f9e0a126c2c3015fc59034001c3b
Obolo Emmanuel <e.obolo@alustudent.com>
2023-07-14 13:25:47 -0700

    bug fixed in 10. Only by 2

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -5,7 +5,7 @@ def divisible_by_2(my_list=[]):
         return None
     else:
         for i in my_list:
-            if my_list[i] % 2 == 0:
+            if i % 2 == 0:
                 new_list.append(True)
             else:
                 new_list.append(False)
