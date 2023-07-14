3fcc330e6d38ce893c1e245c092ec14b8ed6b46d
Obolo Emmanuel <e.obolo@alustudent.com>
2023-07-14 06:44:22 -0700

    bug fixed in 3. Print a list of integers... in reverse!

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    if my_list == None:
-        return 0
+    if my_list is None:
+        return 1
     lengthOfList = len(my_list)
     while lengthOfList >= 1:
         print("{:d}".format(my_list[lengthOfList - 1]))
