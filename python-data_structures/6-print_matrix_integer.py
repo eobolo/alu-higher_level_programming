65d90c4834d629432ee4556ebd513e18265ef86b
Obolo Emmanuel <e.obolo@alustudent.com>
2023-07-14 09:53:43 -0700

    6. Lists of lists = Matrix

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix == None:
+    if matrix is None:
         return 1
     for m_rows in matrix:
         for j, n_columns in enumerate(m_rows):
