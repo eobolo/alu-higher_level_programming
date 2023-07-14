627bb863c53dd3a2d0a28df2904dac3b01c3ce3e
Obolo Emmanuel <e.obolo@alustudent.com>
2023-07-14 12:15:20 -0700

    bug fixed 7. Tuples addition

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -11,10 +11,10 @@ def add_tuple(tuple_a=(), tuple_b=()):
         else:
             tuple_b = (0, 0)
     elif length_a == 1 or length_b == 1:
-        if length_a == 0 and length_b == 0:
+        if length_a == 1 and length_b == 1:
             tuple_a = (tuple_a[0], 0)
             tuple_b = (tuple_b[0], 0)
-        elif length_a == 0:
+        elif length_a == 1:
             tuple_a = (tuple_a[0], 0)
         else:
             tuple_b = (tuple_b[0], 0)
