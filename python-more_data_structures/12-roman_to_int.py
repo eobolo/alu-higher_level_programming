84acd229741b77e0deb8837aaad65488a4e99116
Obolo Emmanuel <e.obolo@alustudent.com>
2023-07-15 16:18:05 -0700

    playing with odd number is mind blowing: bug fixed

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -3,8 +3,8 @@ def roman_to_int(roman_string):
     numerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
                 'C': 100, 'D': 500, 'M': 1000}
     roman_sum = 0
-    if type(roman_string) != "str" or roman_string is None:
-        return 0
+    if type(roman_string) != str or roman_string is None:
+        return roman_sum
 
     if len(roman_string) % 2 == 0:
         for i in range(0, len(roman_string), 2):
