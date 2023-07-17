c4a2a857b8230aa5fb32895c82932a4ecc820939
Obolo Emmanuel <e.obolo@alustudent.com>
2023-07-17 14:09:36 -0700

    bug fixed print 7. Safe integer print with error message

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+import sys
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value, end="\n"))
         return True
     except Exception as err_message:
-        print("Exception: {}".format(err_message))
-    return False
+        print("Exception: {}".format(err_message), file=sys.stderr)
+        return False
