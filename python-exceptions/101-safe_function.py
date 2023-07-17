5994487f3b2d5a50f514ff066d0fb21b47ecfb45
Obolo Emmanuel <e.obolo@alustudent.com>
2023-07-17 14:39:13 -0700

    bug fixed 8. Safe function

--- python-exceptions/101-safe_function.py
+++ python-exceptions/101-safe_function.py
@@ -4,7 +4,7 @@ import sys
 
 def safe_function(fct, *args):
     try:
-        result = fct(args[0], args[1])
+        result = fct(*args)
         return result
     except Exception as err_message:
         print("Exception: {}".format(err_message), file=sys.stderr)
