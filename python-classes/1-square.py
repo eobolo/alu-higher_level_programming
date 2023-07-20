65896de5fcebf33abb612a0804c14715494fca70
Obolo Emmanuel <e.obolo@alustudent.com>
2023-07-20 13:17:39 -0700

    1. Square with size

---
+++ python-classes/1-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+# 1-square.py
+"""
+This is a class module
+that creates a class called
+Square.
+"""
+
+
+class Square:
+    """This is the class Square
+    that has a private instance method
+    private instance method are created
+    with __attribute(variable)name
+
+    """
+    def __init__(self, size):
+        """
+        this is a built-in special python
+        function that help instantiate object/
+        instance of class variables it is also
+        called an initialization method
+        """
+        self.__size = size
