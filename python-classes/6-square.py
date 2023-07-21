90df73075d341b993e370dd98bb7585dedf97f82
Obolo Emmanuel <e.obolo@alustudent.com>
2023-07-21 02:14:29 -0700

    bug fixed 6. Coordinates of a square

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -68,12 +68,12 @@ positive integers")
             elif len(value) > 2 or len(value) < 2:
                 raise TypeError("position must be a tuple of 2 \
 positive integers")
-            elif value[0] < 0 or value[1] < 0:
-                raise TypeError("position must be a tuple of 2 \
-positive integers")
             elif not (isinstance(value[0], int) and isinstance(value[1], int)):
                 raise TypeError("position must be a tuple of 2 \
 positve integers")
+            elif value[0] < 0 or value[1] < 0:
+                raise TypeError("position must be a tuple of 2 \
+positive integers")                
             else:
                 self.__position = value
         except TypeError as e:
