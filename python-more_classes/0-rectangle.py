c0e21f690f12e588c31f1179a48d6224ffe13844
Obolo Emmanuel <e.obolo@alustudent.com>
2023-07-23 10:27:22 -0700

    0. Simple rectangle

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# 0-rectangle.py
+"""This is a python script
+that contains the class Rectangle.
+"""
+
+
+class Rectangle():
+    """This is an empty class
+    rectangle that has not attributes
+    or methods.
+    """
+    pass
