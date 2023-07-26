3f7730e68b61e595bf40cc11bf09b86bbb024d2d
Obolo Emmanuel <e.obolo@alustudent.com>
2023-07-26 13:28:49 -0700

    debugging once more.

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -30,7 +30,6 @@ class MyList(list):
         the instance not in place
         """
         sorted_list = sorted(self)
-        print(sorted_list)
         return sorted_list
 
     def __str__(self):
