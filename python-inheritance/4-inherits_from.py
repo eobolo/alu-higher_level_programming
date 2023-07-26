24479a5f140fccfecb218f7d38c2a8eabdf6238c
Obolo Emmanuel <e.obolo@alustudent.com>
2023-07-26 14:30:44 -0700

    4. Only sub class of

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# 4-inherits_from.py
+"""This is a python module
+thats checks if a class is
+a sub class of another class
+"""
+def inherits_from(obj, a_class):
+    """The function doing the
+    test
+    """
+    if isinstance(obj, a_class) and issubclass(obj.__class__, a_class):
+        return True
+    else:
+        False
