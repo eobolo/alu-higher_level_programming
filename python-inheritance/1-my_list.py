e087a00e1d0c7784dcc78517f965f7e513012291
Obolo Emmanuel <e.obolo@alustudent.com>
2023-07-26 11:01:38 -0700

    1. My list

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+# 1-my_list.py
+"""This is a python module
+that inherits from th built-in
+class List and creates a sorted
+function to sort the list not
+in-place
+"""
+
+
+class MyList(list):
+    """This is my class
+    MyList that inherits from
+    the built in class list
+    an creates a sorted function
+    """
+    # An initialization method of an empty list is done by the parent class
+    def print_sorted(self):
+        """This is the sorted function
+        that sorts the the the instance
+        of the list mylist which inherits
+        the abilities of the class list
+        and the method .sort() and sorts
+        the instance not in place
+        """
+        print(sorted(self))
