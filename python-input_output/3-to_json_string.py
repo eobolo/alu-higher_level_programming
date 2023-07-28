e790c0e7fed9193b04a61814971a590301d2dd99
Obolo Emmanuel <e.obolo@alustudent.com>
2023-07-27 18:37:52 -0700

    3. To JSON string

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import json
+
+# 3-to_json_string.py
+"""This is a python script that
+serializes complex data heirarchies
+using the json module
+"""
+
+
+def to_json_string(my_obj):
+    """This is the function that returns
+    the serialized complex data in strings
+    for better storage in file, data, or sent over
+    to a network connection storage
+    """
+    return json.dumps(my_obj)
