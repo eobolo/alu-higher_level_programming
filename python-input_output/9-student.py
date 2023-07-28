d9a957d473dcc1f837d6c3fad556ac16c9d42d1d
Obolo Emmanuel <e.obolo@alustudent.com>
2023-07-27 21:14:54 -0700

    9. Student to JSON

---
+++ python-input_output/9-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+# 9-student.py
+"""This is a python script that
+creates a class and does json
+serialization and deserialization
+"""
+
+import json
+
+
+class Student():
+    """This is our class student
+    with the init() method and a
+    public method
+    """
+    def __init__(self, first_name, last_name, age):
+        """This is the initialization
+        method with public instance atrributes
+        first-name, last_name, and age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """This public method
+        serializes and deserializes
+        using json
+        """
+        dumps = json.dumps(self.__dict__)
+        loads = json.loads(dumps)
+        return loads
