0d3f3703dbb8e94d7d30b498483a56fc773c5d55
Obolo Emmanuel <e.obolo@alustudent.com>
2023-07-31 11:34:37 -0700

    4. First table

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,7 @@
+-- a script thst creates a table called first_table
+-- in the current database called mysql using
+-- the DBMS called MYSQL SERVER
+CREATE TABLE IF NOT EXISTS first_table(
+	id INT,
+	name VARCHAR(256)
+);
