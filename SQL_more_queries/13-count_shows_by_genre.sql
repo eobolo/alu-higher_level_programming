68c49eba4030bde1f202f9df86da6a4e5b209ff5
Obolo Emmanuel <e.obolo@alustudent.com>
2023-08-01 16:05:56 -0700

    bug fixed in 13. Number of shows by genre

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,7 +2,7 @@
 -- a script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
 -- Each record should display: tv_shows.title - tv_show_genres.genre_id
 -- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
-SELECT tvg.name AS genre, COUNT(tvsg.genre_id) as number_of_shows
+SELECT tvg.name AS genre, COUNT(tvsg.genre_id) AS number_of_shows
 FROM tv_shows AS tvs
 INNER JOIN tv_show_genres AS tvsg
 ON tvsg.show_id = tvs.id
