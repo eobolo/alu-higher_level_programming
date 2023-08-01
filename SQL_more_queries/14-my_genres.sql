33f8995e08b37d2494fac3f80acb0df2cc536499
Obolo Emmanuel <e.obolo@alustudent.com>
2023-08-01 16:14:20 -0700

    bug fixed in 14. My genres

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -2,11 +2,11 @@
 -- a script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
 -- Each record should display: tv_shows.title - tv_show_genres.genre_id
 -- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
-SELECT tvg.name AS genre
+SELECT tvg.name
 FROM tv_shows AS tvs
 INNER JOIN tv_show_genres AS tvsg
 ON tvsg.show_id = tvs.id
 INNER JOIN tv_genres AS tvg
 ON tvg.id = tvsg.genre_id
 WHERE tvs.title = 'Dexter'
-ORDER BY genre ASC;
+ORDER BY tvg.name ASC;
