6f574ca06a117b9947867aedae38413acd4473b9
Obolo Emmanuel <e.obolo@alustudent.com>
2023-08-01 10:37:42 -0700

    bug fixing in 0. My privileges!

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,6 +1,6 @@
 -- a script that lists all privileges of the MySQL users
--- and user_0d_2 on your server (in localhost).
+-- user_0d_1 and user_0d_2 on your server (in localhost).
 -- connect to he mysql server
-mysql -u root -p
+
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
