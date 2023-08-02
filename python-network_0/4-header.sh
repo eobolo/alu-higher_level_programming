8864b7dc929697139fc4b91bae70009bf4654984
Obolo Emmanuel <e.obolo@alustudent.com>
2023-08-02 13:07:01 -0700

    bug fixing 4. cURL headers

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Send the GET request with the specified header and display the body of the response
-curl -s -X GET -H "X-HolbertonSchool-User-Id: 98" "$1" | awk '/^\r?$/ { body=1; next } body { print }'
+# Send the GET request with the specified header, display the body of the response, and extract "OK"
+curl -s -X GET -H "X-HolbertonSchool-User-Id: 98" "$1" | grep -o 'OK'
