e6312c2b24ced28b59332d03e731cb6963c51579
Obolo Emmanuel <e.obolo@alustudent.com>
2023-08-02 10:25:23 -0700

    bug fixing in 0. cURL body size

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
-
-# Check if the URL argument is provided
-if [ -z "$1" ]; then
-  echo "Usage: $0 <URL>"
-  exit 1
-fi
-
-url="$1"
-
-# Send the request using curl and output the size of the response body in bytes
-filesize=$(curl -s -w '%{size_download}' -o /dev/null "$url")
-
-# Display the size of the response body in bytes
-echo "$filesize"
+# Send the request using curl, and output the size of the response body in bytes
+curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
