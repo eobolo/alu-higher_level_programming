f81739fef954379318a995f75775a17548679fa8
Obolo Emmanuel <e.obolo@alustudent.com>
2023-08-02 13:39:04 -0700

    4. cURL headers

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Send the GET request with the specified header, display the body of the response, and ignore the headers
-curl -s -X GET -H "X-HolbertonSchool-User-Id: 98" "$1" | tail -n +13
+# Send the GET request with the specified header and display the body of the response
+curl -s -X GET -H "X-HolbertonSchool-User-Id: 98" "$1" | awk '/^Hello Holberton School!$/{flag=1} flag'
