be634139257ccc6394da7634375cd8cbceba2840
Obolo Emmanuel <e.obolo@alustudent.com>
2023-08-02 13:15:10 -0700

    bug fixing still

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Send the GET request with the specified header, display the body of the response, and extract "OK"
-curl -s -X GET -H "X-HolbertonSchool-User-Id: 98" "$1" | awk '/^\r?$/ { body=1; next } body { print }' | grep -o 'OK'
+# Send the GET request with the specified header, display the response headers, and extract the reason phrase
+curl -s -I -X GET -H "X-HolbertonSchool-User-Id: 98" "$1" | grep 'HTTP/1.1' | awk '{ print $3 }'
