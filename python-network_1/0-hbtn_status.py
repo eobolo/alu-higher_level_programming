ce303102dc1f8ab0e76e8fb3a318866d5f32505e
Obolo Emmanuel <e.obolo@alustudent.com>
2023-08-03 16:43:09 -0700

    0. What's my status? #0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -9,7 +9,11 @@ using the urllib library
 import urllib.request
 
 
-url = "https://alu-intranet.hbtn.io/status"
-request = urllib.request.Request(url)
-with urllib.request.urlopen(request) as response:
-    print(response.read())
+url = 'https://alu-intranet.hbtn.io/status'
+with urllib.request.urlopen(url) as response:
+    response_body = response.read()
+    response_text = response_body.decode('utf-8')
+    print("Body response:")
+    print("\t- type:", type(response_body))
+    print("\t- content:", response_body)
+    print("\t- utf8 content:", response_text)
