692db57f994365216f0df7629f16cbf2abb88c1b
Obolo Emmanuel <e.obolo@alustudent.com>
2023-08-04 08:54:46 +0000

    new change

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send the GET request with the specified header and display the body of the response
-curl -s -i "$1" | grep -E "HTTP" | awk '{print $3}'
+curl -s -i "$1" | grep -E "HTTP" | cut --delimiter=" " -f 3 | tr -d '\r\n'
