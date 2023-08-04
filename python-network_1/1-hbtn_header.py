cb109bda65da3cfe893bb23efc23f15b6cb3a3d4
Obolo Emmanuel <e.obolo@alustudent.com>
2023-08-03 17:47:13 -0700

    bug fixing

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
-import sys
-import urllib.request
-"""This module is a script
+"""
+This module is a script
 that takes in a URL, sends a request
 to the URL and displays the value of
 the X-Request-Id variable found in
 the header of the response.
 """
+
+
+import urllib.request
+import sys
 
 
 url = sys.argv[1]
