99790abe739bfa828a6af99b09dae0fb42820ea3
Obolo Emmanuel <e.obolo@alustudent.com>
2023-08-03 18:01:16 -0700

    bug fixing 1. Response header value #0

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
-# 1-hbtn_header.py
-"""This a python module
-that takes a url and fetches
-and display the value of an
-header
+# 0-hbtn_status.py
+"""This module or script
+fetches https://alu-intranet.hbtn.io/status
+using the urllib library
 """
 
 
-import urllib.request
+from urllib import request, error
 import sys
-
-
-url = sys.argv[1]
-request = urllib.request.Request(url)
-with urllib.request.urlopen(request) as response:
-    print(response.headers["X-Request-Id"])
+try:
+    url = sys.argv[1]
+    with request.urlopen(url) as response:
+        print(response.headers['X-Request-Id'])
+except error.URLError:
+    custom_status = bytes("Custom status", encoding="utf-8")
+    custom_status_text = custom_status.decode('utf-8')
+    print("Body response:")
+    print("\t- type:", type(custom_status))
+    print("\t- content:", custom_status)
+    print("\t- utf8 content:", custom_status_text)
