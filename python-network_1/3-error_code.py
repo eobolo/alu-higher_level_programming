af36358bcb0842f107121bbaf9965acd422b4e05
Obolo Emmanuel <e.obolo@alustudent.com>
2023-08-04 03:24:31 -0700

    3. Error code #0

--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -4,38 +4,17 @@
 Usage: ./3-error_code.py <URL>
   - Handles HTTP errors.
 """
-
-
-import urllib.request
-import urllib.error
 import sys
+import urllib.error
+import urllib.request
 
-def fetch_and_display_url_body(url):
-    """
-    Fetches the body of a URL response and displays it.
-
-    Args:
-        url (str): The URL to fetch the response from.
-
-    Returns:
-        str: The decoded body of the response.
-
-    Raises:
-        urllib.error.HTTPError: If an HTTP error occurs (e.g., 404, 500).
-    """
-    try:
-        with urllib.request.urlopen(url) as response:
-            body = response.read().decode('utf-8')
-            print("Response Body:")
-            print(body)
-            return body
-    except urllib.error.HTTPError as e:
-        print(f"Error code: {e.code}")
-        return ""
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python3 script_name.py <url>")
-    else:
-        url = sys.argv[1]
-        fetch_and_display_url_body(url)
+    url = sys.argv[1]
+
+    request = urllib.request.Request(url)
+    try:
+        with urllib.request.urlopen(request) as response:
+            print(response.read().decode("ascii"))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
