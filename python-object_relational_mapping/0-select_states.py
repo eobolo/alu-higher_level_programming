45234e7e05e5d7630bda4aa93635a48e2b70349f
Obolo Emmanuel <e.obolo@alustudent.com>
2023-08-17 12:27:45 -0700

    fugured out that when you use import sys you should make that file not a module inorder to see your documentation

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -15,8 +15,6 @@ Your code should not be executed when imported
 if __name__ == "__main__":
     import MySQLdb
     import sys
-
-
     database = MySQLdb.connect(host="localhost", port=3306,
                                user=sys.argv[1], password=sys.argv[2],
                                database=sys.argv[3],
