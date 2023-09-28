b8753015a7d6d4e2a6f73d7ff8fea0b34a5d21d2
Obolo Emmanuel <e.obolo@alustudent.com>
2023-09-28 02:06:20 -0700

    bug fixed 2. Rectangle #2

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const Rectangle = class {
   constructor (w, h) {
-    if (w <= 0 || h <= 0 || h === undefined) {
+    if (w <= 0 || w === undefined || h <= 0 || h === undefined) {
       // This Block does nothing
     } else {
       this.width = w;
