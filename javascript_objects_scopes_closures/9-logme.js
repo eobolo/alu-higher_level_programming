18e7fb7ebcacbd843c809c0438ab4a99f01ca922
Obolo Emmanuel <e.obolo@alustudent.com>
2023-09-29 09:24:42 -0700

    9. Log me done again

--- javascript_objects_scopes_closures/9-logme.js
+++ javascript_objects_scopes_closures/9-logme.js
@@ -8,7 +8,7 @@ class Calls {
     return Calls.number;
   }
 }
-Calls.number = -1
+Calls.number = -1;
 exports.logMe = function (item) {
   const count = new Calls().call();
   console.log(`${count}: ${item}`);
