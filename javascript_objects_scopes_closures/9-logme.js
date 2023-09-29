9fee4716fc564d0473d3a5383c0814f49ff9616d
Obolo Emmanuel <e.obolo@alustudent.com>
2023-09-29 09:15:50 -0700

    9. Log me done again

--- javascript_objects_scopes_closures/9-logme.js
+++ javascript_objects_scopes_closures/9-logme.js
@@ -1,30 +1,15 @@
 #!/usr/bin/node
+class Calls {
+  static number = -1;
+  constructor () {
+    Calls.number++;
+  }
+
+  call () {
+    return Calls.number;
+  }
+}
 exports.logMe = function (item) {
-  const pathName = 'Loggings.txt';
-  const fs = require('fs');
-  const readData = parseInt(readFromFile(pathName, fs));
-  console.log(`${readData}: ${item}`);
-  const writeData = readData + 1;
-  writeToFile(pathName, writeData.toString(), fs);
+  const count = new Calls().call();
+  console.log(`${count}: ${item}`);
 };
-
-function readFromFile (pathName, fs) {
-  let data;
-  try {
-    data = fs.readFileSync(pathName, 'utf-8');
-    return data;
-  } catch (err) {
-    data = 0;
-    return data;
-  }
-}
-
-function writeToFile (pathName, dataToWrite, fs) {
-  try {
-    fs.writeFileSync(pathName, dataToWrite, 'utf-8');
-    return 0;
-  } catch (err) {
-    // do nothing
-    return 1;
-  }
-}
