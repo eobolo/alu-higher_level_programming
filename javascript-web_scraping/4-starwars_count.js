13c18e880bb7e9964c86f68fa0d7b492a98ca145
Obolo Emmanuel <e.obolo@alustudent.com>
2023-10-03 14:00:44 -0700

    bug fixed

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,32 +1,12 @@
 #!/usr/bin/node
-const { argv } = require('process');
 const request = require('request');
-
-if (argv[2]) {
-  const options = {
-    method: 'GET',
-    uri: argv[2]
-  };
-  request(argv[2], options, (err, response, body) => {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    const id = 18;
-    try {
-      const parsedData = JSON.parse(body);
-      const url = parsedData.results[0].characters[id - 3];
-      request(url, (err, response, body) => {
-        if (err) {
-          console.log(err);
-          return;
-        }
-        console.log(JSON.parse(body).films.length);
-      });
-    } catch (error) {
-      console.log(error);
-    }
-  });
-} else {
-  console.log('An error occured missing url command line arg.');
-}
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const results = JSON.parse(body).results;
+    console.log(results.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1
+        : count;
+    }, 0));
+  }
+});
