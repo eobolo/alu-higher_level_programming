59b3e7153f272b7be03ddcbd50f92832824d3a3e
Obolo Emmanuel <96441399+olugbeminiyi2000@users.noreply.github.com>
2023-10-05 13:05:20 +0300

    2. Click and turn red

---
+++ javascript-web_jquery/2-script.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const divId = document.querySelector('DIV#red_header');
+const header = document.querySelector('header');
+divId.addEventListener('click', (event) => {
+  header.style.color = '#FF0000'';
+});
