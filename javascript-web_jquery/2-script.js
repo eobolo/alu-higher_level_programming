b7b366b8371d0c098c9933604dbea13dcb294a45
Obolo Emmanuel <e.obolo@alustudent.com>
2023-10-05 03:08:36 -0700

    permissions given to 2. Click and turn red files

--- javascript-web_jquery/2-script.js
+++ javascript-web_jquery/2-script.js
@@ -2,5 +2,5 @@
 const divId = document.querySelector('DIV#red_header');
 const header = document.querySelector('header');
 divId.addEventListener('click', (event) => {
-  header.style.color = '#FF0000'';
+  header.style.color = '#FF0000';
 });
